66f93513f3bd8545d1067ef8e66b5d6856e884f5
pasha <pasha@coderiver.com.ua>
2019-03-24 12:46:48 +0200

    create index page

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pawkat
+ * Date: 2019-03-24
+ * Time: 12:38
+ */
+
+
+$title = 'Geekbrains PHP Courses';
+$h1 = 'Geekbrains PHP Courses';
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title?></title>
+</head>
+<body>
+<h1><?php echo  $h1 ?></h1>
+
+</body>
+</html>
+
+
